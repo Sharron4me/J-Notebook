c8cf880e3e0751ad8df3159e909391d084ce67ee
Mohammad Ummair <mohdummair007@gmail.com>
2019-03-20 18:02:37 +0530

    New File Name Exception handler

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -14,10 +15,12 @@ public class Controller {
     @FXML
     public ListView filelist;
     public TextField file_pro_name;
+    public Label error_message;
 
     public void createnew() {
 
         //dATABASE BLOCK
+
         try{
             Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Sherrinford\\Documents\\Jnotebook\\testjava.db");
             try {
@@ -29,36 +32,49 @@ public class Controller {
             catch(Exception e){
                 System.out.println("Table already exits "+e);
             }
-            String sql_insert="Insert INTO file_Name (Address,Parent_file) values (?,?)";
-            PreparedStatement ps = null;
-            ps=conn.prepareStatement(sql_insert);
-            ps.setString(1, "C:");
-            ps.setString(2, file_pro_name.getText());
-//            ps.setString(3, Name_of_customer);
-//            ps.setString(4, Mode_of_payment);
-            ps.execute();
-            ps.close();
+
+            String Check="SELECT COUNT(*) FROM  file_Name WHERE Parent_file=?";
+            PreparedStatement chk_statement = null;
+            chk_statement=conn.prepareStatement(Check);
+            chk_statement.setString(1,file_pro_name.getText());
+            ResultSet rs=chk_statement.executeQuery();
+            int counter =rs.getInt("COUNT(*)");
+            chk_statement.close();
+            if(counter>0){
+                error_message.setVisible(true);
+                error_message.setText("File Name Already Exists!! Please Choose another one");
+            }
+            else {
+                error_message.setVisible(false);
+                String sql_insert = "Insert INTO file_Name (Address,Parent_file) values (?,?)";
+                PreparedStatement ps = null;
+                ps = conn.prepareStatement(sql_insert);
+                ps.setString(1, "C:\\"+file_pro_name.getText()+".java");
+                ps.setString(2, file_pro_name.getText());
+                ps.execute();
+                ps.close();
+                try
+
+                {
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("open.fxml"));
+                    Parent root1 = (Parent) fxmlLoader.load();
+                    Stage stage = new Stage();
+                    stage.setTitle("second page");
+                    stage.setScene(new Scene(root1));
+                    stage.show();
+
+                }
+                catch (Exception e)
+                {
+                    System.out.println("Something Went Wrong:"+e);
+                }
+            }
             conn.close();
         }
         catch(SQLException e){
             System.out.println("Something Went Wrong!  "+e);
         }
-        try
 
-        {
-
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("open.fxml"));
-            Parent root1 = (Parent) fxmlLoader.load();
-            Stage stage = new Stage();
-            stage.setTitle("second page");
-            stage.setScene(new Scene(root1));
-            stage.show();
-
-        }
-        catch (Exception e)
-        {
-            System.out.println("Something Went Wrong");
-        }
     }
 
     public void refresh() {
